23e08dfbf8440e7da815f66ef6c6e4370c5a4733
Ronaldo <ronaldov8318@gmail.com>
2019-06-16 02:07:39 -0300

    Mudança de teste

--- docs/js/routes.js
+++ docs/js/routes.js
@@ -3,7 +3,7 @@
 /* Routes aplication config.
 /* 
 /* Utilizado diretiva uiRouter do AngularJs.     
-/*   
+/*   12132132
 /* Desenvolvido por Matheus Ferreira <maathe.f@gmail.com>
 /*
 /* ****************************************************** */
