0536ba6854d1febd84487eaeb9809aa910850632
Matheus Ferreira <maathe.f@gmail.com>
2021-07-31 02:58:27 -0400

    Layout bug fix

--- docs/js/translation.js
+++ docs/js/translation.js
@@ -2,16 +2,20 @@ moduleApp.config(['$translateProvider', function ($translateProvider) {
   
     $translateProvider.translations('en', {
       'MENU_TICKETS': 'Buy Tickets',
+      'HOME_TITTLE': 'Synchronizing psytrance in Florida',
       'ABOUT_TITTLE': 'About us',
       
-      'ABOUT_SUBTITTLE': 'Synchronizing the culture of psytrance'
+      'ABOUT_SUBTITTLE': 'Synchronizing the culture of psytrance',
+      'HOME_SUBTITTLE': 'Description about our festival'
     });
    
     $translateProvider.translations('br', {
       'MENU_TICKETS': 'Ingressos',
+      'HOME_TITTLE': 'Synchronizando psytrance na Florida',
       'ABOUT_TITTLE': 'Sobre nós',
       
-      'ABOUT_SUBTITTLE': 'Sincronizando a cultura psytrance.'    
+      'ABOUT_SUBTITTLE': 'Sincronizando a cultura psytrance.',
+      'HOME_SUBTITTLE': 'Descricao do festival'    
     });
 
     $translateProvider.preferredLanguage('en');
